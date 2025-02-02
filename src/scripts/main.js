7c1d824bce236e119b4d402b678ea5e25b87e30f
Nadiia Bulmak <nadgaval@gmail.com>
2025-02-02 17:33:04 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+const population = [...document.querySelectorAll('.population')];
+let totalPopulation = 0;
+
+for (const pop of population) {
+  totalPopulation += +pop.textContent.split(',').join('');
+}
+
+const average = totalPopulation / population.length;
+
+function changeData() {
+  document.querySelector('.total-population').textContent =
+    `${totalPopulation.toLocaleString()}`;
+
+  document.querySelector('.average-population').textContent =
+    `${average.toLocaleString()}`;
+}
+
+changeData();
